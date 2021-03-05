e70840dd280409f7a6287d815677f6f8499f851e
dziulek <99peterstone@gmail.com>
2021-03-05 12:23:22 +0100

    added sfml library

--- src/route.hpp
+++ src/route.hpp
@@ -44,17 +44,7 @@ class Route{
 
             return std::max(succ.e, prec.beginTime + Customer::dist(succ, *prec.customer));
         }
-        static inline float newBeginTime(const Customer &prec, const Customer &succ, const float beginTimePrec){
 
-        inline float newBeginTime(const unsigned int i){
-
-            return std::max(this->route[i].customer->e,
-                            this->route[i - 1].beginTime + Customer::dist(*route[i - 1].customer, *route[i].customer));
-        }
-        static inline float newBeginTime(const routeCustomer &prec, const Customer &succ){
-
-            return std::max(succ.e, prec.beginTime + Customer::dist(succ, *prec.customer));
-        }
         static inline float newBeginTime(const Customer &prec, const Customer &succ, const float beginTimePrec){
 
             return std::max(succ.e, beginTimePrec + Customer::dist(succ, prec));
