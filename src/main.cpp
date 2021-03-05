3431e003484df115707eafb13bab9d213a65787b
dziulek <99peterstone@gmail.com>
2021-03-05 12:41:00 +0100

    graphics: sfml core

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 #include <string>
 
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+
+void draw_current_solution(sf::RenderWindow & window, sf::View & view){
+
+}
+
+void draw_solution_info(sf::RenderWindow & window, sf::View & view){
+
+}
 
 int main(int argc, char * argv[]){
-    
+
+    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode(600,800), "simmulated annealing");
+    sf::View view;
+    view.setViewport(sf::FloatRect(0.0f, 0.0f, 1.0f, 1.0f));
+    window.setView(view);
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+        window.setFramerateLimit(60);
+        window.display();
+    }
+
+    return 1;
 }
